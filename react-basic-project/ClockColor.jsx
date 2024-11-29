b6c752e25dd06a896bb561b8feacff284615b753
faizanalim <faizan.23@hotmail.com>
2024-11-29 20:57:21 +0500

    change clock color  by passing props

--- react-basic-project/ClockColor.jsx
+++ react-basic-project/ClockColor.jsx
@@ -1,3 +1,28 @@
-function ClockColor() {}
+import { useEffect } from "react";
+import { useState } from "react";
+
+const ClockColor = ({ color }) => {
+  const [clock, setclock] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      setclock(new Date().toLocaleTimeString());
+    }, 1000);
+  });
+
+  return (
+    <>
+      <h1
+        style={{
+          color: color,
+          background: "#000",
+          width: "170px",
+          borderRadius: "5px",
+        }}
+      >
+        {clock}
+      </h1>
+    </>
+  );
+};
 
 export default ClockColor;
--- react-basic-project/src/App.jsx
+++ react-basic-project/src/App.jsx
@@ -1,10 +1,18 @@
-import ResuseComponet from "./ReuseComponent";
+import { useState } from "react";
+import ClockColor from "../ClockColor";
 
 function App() {
+  const [color, setcolor] = useState("red");
   return (
-    <>
-      <ResuseComponet></ResuseComponet>
-    </>
+    <div>
+      <h1>Digital Clock in React</h1>
+      <select onChange={(event) => setcolor(event.target.value)}>
+        <option value="red">Red</option>
+        <option value="Blue">Blue</option>
+        <option value="green">Green</option>
+      </select>
+      <ClockColor color={color}></ClockColor>
+    </div>
   );
 }
 
